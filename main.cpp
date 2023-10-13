26915d7a7adbdf2c5442ffd2785d25244bca844e
email <fitriahidayanti2122@gmail.com>
2023-10-13 18:01:14 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "---------------------------------------------------------------------------------------" << endl << endl ;
+    cout << "nama: Fitria Hidayanti" << endl << endl;
+    cout << "NPM: 2310631170085"<< endl << endl;
+    cout << "kelas : A - Informatika" << endl << endl ;
+    cout << "cita-cita: guru" << endl << endl ;
+    cout << "motivasi masuk informatika : ingin mempelajari lebih lanjut tentang informatika" << endl ;
+    cout << "---------------------------------------------------------------------------------------" << endl ;
+    return 1;
+}
